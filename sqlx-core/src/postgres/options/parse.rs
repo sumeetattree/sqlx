69594acdc6be538b070c46632d22f1e299f298a3
ThibsG <thibsg@pm.me>
2022-05-01 10:58:48 +0200

    Add client ssl tests for PostgreSQL

--- sqlx-core/src/postgres/options/parse.rs
+++ sqlx-core/src/postgres/options/parse.rs
@@ -57,9 +57,9 @@ impl FromStr for PgConnectOptions {
                     options = options.ssl_root_cert(&*value);
                 }
 
-                "sslcert" => options = options.ssl_client_cert(&*value),
+                "sslcert" | "ssl-cert" => options = options.ssl_client_cert(&*value),
 
-                "sslkey" => options = options.ssl_client_key(&*value),
+                "sslkey" | "ssl-key" => options = options.ssl_client_key(&*value),
 
                 "statement-cache-capacity" => {
                     options =
